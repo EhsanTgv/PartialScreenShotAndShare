c74689fb4ef90a2c524a94838c36a09cc5b2a94d
EhsanTgv <EhsanTgv@gmail.com>
2021-02-06 14:03:06 +0330

    Taking Partial ScreenShot

--- app/src/main/java/com/taghavi/partialscreenshotandshare/MainActivity.kt
+++ app/src/main/java/com/taghavi/partialscreenshotandshare/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.taghavi.partialscreenshotandshare
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.taghavi.partialscreenshotandshare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            sampleConstraintLayout.setOnClickListener {
+                val bitmap = getBitmapFromView(sampleConstraintLayout)
+                showingResult.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
     }
 }
